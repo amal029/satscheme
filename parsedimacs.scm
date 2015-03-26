61b1ebc3e39375a3cafa83ffe99c0e265307ff6e
Avinash Malik <avinash.malik@auckland.ac.nz>
2015-03-26 14:17:01 +1300

    Started

--- parsedimacs.scm
+++ parsedimacs.scm
@@ -10,3 +10,30 @@
 ;;; Then following lines should have only comments or numbers.
 
 ;;; A negative number means not (remember).
+
+(require-extension s)
+(require-extension section-combinators)
+
+(define (parse-dimacs p)
+  (let*
+      ((lines (filter
+	       (lambda (l)
+		 (not (or (s-starts-with? "c" l) (eq? l '())))) (read-lines p)))
+       (pline (filter
+	       (lambda (l)
+		 (s-starts-with? "p" l)) lines))
+       (lines (filter
+	       (lambda (l)
+		 (not (s-starts-with? "p" l))) lines))
+       (lines (flatten (map (left-section s-split " ") lines))))
+    (cond
+     ((eq? pline '())
+      (begin
+	(display "Problem specification incomplete")
+	(newline)
+	(abort 'exn)))
+     (else
+      (display lines)))))
+
+(define (main file-name)
+  (call-with-input-file file-name parse-dimacs))
