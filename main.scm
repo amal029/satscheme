032765073d3ca3e44ec18b0d8b064db0590c90fb
Avinash Malik <amalik@scss.tcd.ie>
2015-03-28 23:17:39 +1300

    Fixed the usage message

--- main.scm
+++ main.scm
@@ -5,19 +5,22 @@
 (require-extension getopt-long)
 (require-extension section-combinators)
 
+
 (define grammar
   `((file "Name of the input file in DIMACS cnf format"
 	  (single-char #\f)
 	  (value #t)
-	  (required #t))
+	  (required #f))
     (solve "Solve the sat problem"
 	  (single-char #\s)
 	  (value #f)
 	  (required #f))
-    (time "Timeout for the solver in seconds [0 mean never timeout!]"
+    (time "Timeout for the solver in seconds [0 means never timeout, default:0]"
 	  (single-char #\t)
 	  (value #t)
-	  (required #f))))
+	  (required #f))
+    (help "Print this help message"
+	  (single-char #\h))))
 
 (define (myparse timeout-value solve fn)
   (let-values (((ss clauses) (parse-dimacs fn)))
@@ -39,11 +42,12 @@
 (let* ((options (getopt-long (argv) grammar))
        (fn (alist-ref 'file options))
        (s (alist-ref 'solve options))
+       (h (alist-ref 'help options))
        (t (alist-ref 'time options))
        (tv (if (equal? t #f) "0"
 	       t)))
-  (if (equal? fn #f)
-      (usage)
+  (if (or (equal? h #t) (equal? fn #f))
+      (display (usage grammar))
       (if (list? fn)
 	  (map (left-section myparse s) fn)
 	  (myparse tv s fn))))
